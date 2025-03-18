f274a5290fc235cc458050dffc37e9d0750ed95b
MarlonChi <marlonchiodelli@hotmail.com>
2025-03-17 22:17:58 -0300

    feat: video explanation

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import { Header } from "./components/landing-page/header";
 import { Hero } from "./components/landing-page/hero";
+import { VideoExplanation } from "./components/landing-page/video-explanation";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto ">
       <Header />
       <Hero />
-
-      {/* 
       <VideoExplanation />
-      <Pricing />
+      {/* <Pricing />
       <FAQ /> */}
     </div>
   );
