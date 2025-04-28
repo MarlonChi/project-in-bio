c267e3a2c1274c40f75fbf70e863733cf9c2bc3e
MarlonChi <marlonchiodelli@hotmail.com>
2025-04-28 20:38:05 -0300

    chore: add mixpanel token

--- app/lib/mixpanel.ts
+++ app/lib/mixpanel.ts
@@ -1,6 +1,6 @@
 import Mixpanel from "mixpanel";
 
-const mixpanelEvent = Mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN!);
+const mixpanelEvent = Mixpanel.init("3c337dd0b8b671df33179c03e35caea2");
 
 export function trackServerEvent(eventName: string, properties: any) {
   if (process.env.NODE_ENV === "development") return;
